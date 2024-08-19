c6e5c4cc5c7a6bbce606fdce5b400906bd5da099
Harsha7668 <132264211+Harsha7668@users.noreply.github.com>
2024-08-19 18:23:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,4 +7,5 @@ API_HASH = os.environ.get("API_HASH", "191bf5ae7a6c39771e7b13cf4ffd1279")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "7412278588:AAHmk19iP3uK79OglBISjicbl70TD6i9wEc")
 ADMIN = int(os.environ.get("ADMIN", "6469754522")) 
 FSUB_CHANNEL = os.environ.get("FSUB_CHANNEL", "Sunrises24BotUpdates")
-
+DATABASE_URI = os.environ.get("DATABASE_URI", "mongodb+srv://INFINITYRENAME24BOT:INFINITYRENAME24BOT@cluster0.colorb9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "Cluster0")
