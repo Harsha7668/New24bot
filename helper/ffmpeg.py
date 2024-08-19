bf31627492d33786ad7b5d66e882f6783a5bbf68
Harsha7668 <132264211+Harsha7668@users.noreply.github.com>
2024-08-19 17:53:06 +0530

    Create ffmpeg.py

---
+++ helper/ffmpeg.py
@@ -0,0 +1,25 @@
+import subprocess
+
+# Function to apply metadata to the file using FFmpeg
+def change_video_metadata(input_path, video_title, audio_title, subtitle_title, output_path):
+    command = [
+        'ffmpeg',
+        '-i', input_path,
+        '-metadata', f'title={video_title}',
+        '-metadata:s:v', f'title={video_title}',
+        '-metadata:s:a', f'title={audio_title}',
+        '-metadata:s:s', f'title={subtitle_title}',
+        '-map', '0:v?',
+        '-map', '0:a?',
+        '-map', '0:s?',
+        '-c:v', 'copy',
+        '-c:a', 'copy',
+        '-c:s', 'copy',
+        output_path,
+        '-y'
+    ]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        raise Exception(f"FFmpeg error: {stderr.decode('utf-8')}")
+
