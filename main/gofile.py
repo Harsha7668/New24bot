b1c247987796757728704a7a12510e2dd022ed13
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2024-08-20 14:38:36 +0530

    Update gofile.py

--- main/gofile.py
+++ main/gofile.py
@@ -1,8 +1,14 @@
 import aiohttp
 import os
 import time
+from helper.database import db
 
 async def gofile_upload(file_path, file_name, gofile_api_key):
+    gofile_api_key = await db.get_gofile_api_key(user_id)
+
+    if not gofile_api_key:
+        return await msg.reply_text("Gofile API key is not set. Use /gofilesetup {your_api_key} to set it.")
+        
     upload_url = "https://store1.gofile.io/uploadFile"
 
     try:
